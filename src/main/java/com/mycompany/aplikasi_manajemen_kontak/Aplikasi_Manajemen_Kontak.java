44a93b98fa1277460ca03f6682e1e2fe76a46791
marzuki23 <muh.idrismarzuki2301@gmail.com>
2025-06-16 09:31:02 +0700

    first commit

---
+++ src/main/java/com/mycompany/aplikasi_manajemen_kontak/Aplikasi_Manajemen_Kontak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasi_manajemen_kontak;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Aplikasi_Manajemen_Kontak {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
